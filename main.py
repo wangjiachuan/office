661c07944c04e864b295bcd2c0b8df59cb652817
wangjiachuan <93126721@qq.com>
2016-03-24 14:01:55 +0800

    use python27 to control

--- main.py
+++ main.py
@@ -2,14 +2,27 @@
 import sys
 import os
 import time
+import mail_crawler
 
-class DailyRegisterMain(object):           
+class DailyRegisterMain(object):
+    def should_run(self):
+        result = mail_crawler.get_mails()
+        for i in result:
+            print ('主题：%s'%(i['subject'].encode('utf8')))
+            if i['subject'].encode('utf8') == "1":
+                return False
+    
+        return True
+    
     def main(self):
         for i in range(50000):
             print('-'*30)
             print("cycle is :{0}".format(i+1))
             os.chdir(os.getcwd())
-            os.system(r"C:\Python34\python.exe D:\office\message\sendmessage.py")
+            if self.should_run()== True:
+                os.system(r"C:\Python34\python.exe D:\office\message\sendmessage.py")
+            else:
+                print("find stop mail in 163 mail box,quit")
             print('-'*30)
             print("cycle done,start another cycle:{0}".format(i+1))
             time.sleep(60)
@@ -19,3 +32,4 @@ if __name__ == '__main__':
 
     myRegister = DailyRegisterMain()
     myRegister.main()
+
--- readme.txt
+++ readme.txt
@@ -1,2 +1,2 @@
 open windows power shell 
-C:\Python34\python.exe D:\office\message\main.py
+C:\Python27\python.exe D:\office\message\main.py
