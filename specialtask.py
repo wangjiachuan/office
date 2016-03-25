001b9c156049adba7c84552b73ace497e8684f63
wangjiachuan <93126721@qq.com>
2016-03-25 11:46:36 +0800

    f

--- specialtask.py
+++ specialtask.py
@@ -9,6 +9,10 @@ class SpecialTask(object):
     
     def main(self):
         print("hello,i am special task")
+        os.system("git pull git@github.com:wangjiachuan/office.git")
+        cmd = "copy C:"+os.sep+"Users"+os.sep+"nfs"+os.sep+"*.py"+"  "+"D:"+os.sep+"office"+os.sep+"message"+os.sep+"."
+        print(cmd)
+        os.system(cmd)
         
 
         
