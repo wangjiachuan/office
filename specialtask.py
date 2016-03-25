de5d043b3434489f76680024e5b29058245a800f
wangjiachuan <93126721@qq.com>
2016-03-25 11:04:30 +0800

    f

--- specialtask.py
+++ specialtask.py
@@ -2,13 +2,16 @@
 import sys
 import os
 import time
-import mail_crawler
+
+
 
 class SpecialTask(object):
     
     def main(self):
         print("hello,i am special task")
-        pass
+        
+
+        
 
 
 if __name__ == '__main__':
